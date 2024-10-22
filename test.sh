491122cb53d29641e3201fba70d006cc79f143ec
wilm281 <173095437+wilm281@users.noreply.github.com>
2024-10-22 04:30:33 -0500

    a

---
+++ test.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+os=$(uname)
+dir="$(pwd)"
+bin="$(pwd)/$(uname)"
+get_device_mode() {
+    if [ "$os" = "Darwin" ]; then
+        apples="$(system_profiler SPUSBDataType 2> /dev/null | grep -B1 'Vendor ID: 0x05ac' | grep 'Product ID:' | cut -dx -f2 | cut -d' ' -f1 | tail -r)"
+    elif [ "$os" = "Linux" ]; then
+        apples="$(lsusb | cut -d' ' -f6 | grep '05ac:' | cut -d: -f2)"
+    fi
+    local device_count=0
+    local usbserials=""
+    for apple in $apples; do
+        case "$apple" in
+            12a8|12aa|12ab)
+            device_mode=normal
+            device_count=$((device_count+1))
+            ;;
+            1281)
+            device_mode=recovery
+            device_count=$((device_count+1))
+            ;;
+            1227)
+            device_mode=dfu
+            device_count=$((device_count+1))
+            ;;
+            1222)
+            device_mode=diag
+            device_count=$((device_count+1))
+            ;;
+            1338)
+            device_mode=checkra1n_stage2
+            device_count=$((device_count+1))
+            ;;
+            4141)
+            device_mode=pongo
+            device_count=$((device_count+1))
+            ;;
+        esac
+    done
+    if [ "$device_count" = "0" ]; then
+        device_mode=none
+    elif [ "$device_count" -ge "2" ]; then
+        error "Please attach only one device" > /dev/tty
+        kill -30 0
+        exit 1;
+    fi
+    if [ "$os" = "Linux" ]; then
+        usbserials=$(cat /sys/bus/usb/devices/*/serial)
+    elif [ "$os" = "Darwin" ]; then
+        usbserials=$(system_profiler SPUSBDataType 2> /dev/null | grep 'Serial Number' | cut -d: -f2- | sed 's/ //')
+    fi
+    if grep -qE '(ramdisk tool|SSHRD_Script) (Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) [0-9]{1,2} [0-9]{4} [0-9]{2}:[0-9]{2}:[0-9]{2}' <<< "$usbserials"; then
+        device_mode=ramdisk
+    fi
+    echo "$device_mode"
+}
+sudo killall -STOP AMPDevicesAgent AMPDeviceDiscoveryAgent MobileDeviceUpdater
+sudo killall -STOP -c usbd
+while (true);
+do
+    if ! (system_profiler SPUSBDataType 2> /dev/null | grep ' Apple Mobile Device (DFU Mode)' >> /dev/null); then
+        echo "Please connect a device in dfu mode to continue"
+    fi
+    if [[ "$(get_device_mode)" == "pongo" ]]; then
+        bash -c "nohup sh -c '"$bin"/pongoterm <cmd.txt &' > /dev/null &"
+        exit 0
+    fi 
+    "$bin"/USB\ Prober.app/Contents/Resources/reenumerate -v 0x05ac,0x1227
+    sleep 1
+    while ! (system_profiler SPUSBDataType 2> /dev/null | grep ' Apple Mobile Device (DFU Mode)' >> /dev/null);
+    do
+        if [[ "$(get_device_mode)" == "pongo" ]]; then
+            bash -c "nohup sh -c '"$bin"/pongoterm <cmd.txt &' > /dev/null &"
+            exit 0
+        fi 
+        echo "Please reconnect your usb cable"
+        sleep 1
+    done
+    "$bin"/irecovery -q | grep NONC
+    "$bin"/timeout 12 "$bin"/openra1n "$bin"/Pongo.bin
+    #"$bin"/USB\ Prober.app/Contents/Resources/reenumerate -v 0x05ac,0x1227
+    #sleep 1
+    #sleep 2
+    #if (system_profiler SPUSBDataType 2> /dev/null | grep ' Apple Mobile Device (DFU Mode)' >> /dev/null); then
+    #    "$bin"/openra1n3
+    #fi
+    "$bin"/timeout 4 "$bin"/openra1n "$bin"/Pongo.bin boot
+    if [[ ! "$?" == "0" ]]; then
+        #exit 0
+        echo "Exploit failed, please reconnect device in dfu mode to continue"
+    fi
+done
---
+++ test2.sh
@@ -0,0 +1,5 @@
+while (true);
+do
+./Darwin/openra1n2 ./Darwin/Pongo.bin boot
+sleep 1
+done
